8daf81f8e1b14810b3866cb2f2a2071794c40f4d
Coo.King <39448470+CooKingThe1st@users.noreply.github.com>
2023-08-09 15:40:09 +0700

    Add referee controller
    Add referee C++ code
    Add 10 log file for result draw

---
+++ referee_coach_ver_3/build/release/referee_coach_ver3.d
@@ -0,0 +1,11 @@
+build/release/referee_coach_ver3.o: referee_coach_ver3.cpp \
+ C:\Program\ Files\Webots/include/controller/c/webots/display.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/keyboard.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/emitter.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/receiver.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/supervisor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/contact_point.h \
+ basic_strategy.h basic_math_and_OBS.h
